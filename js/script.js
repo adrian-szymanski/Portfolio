1cf07ff6c7f1577604ff7781bf5cafa1d9c53a63
Adrian Szymanski <adrian.szymanski89@gmail.com>
2019-05-21 21:36:36 +0200

    CSS updates
    New background, fonts, animations testing

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('#topRow').css('display', 'none').fadeIn(3000);
+  });
+  
+  $(document).ready(function () {
+    $('#mainRow').css('display', 'none').fadeIn(6000);
+  });
+
+  $(document).ready(function () {
+    $('#bottomBar').css('display', 'none').fadeIn(3000);
+  });
+
+  $(document).ready(function () {
+    $('#copyright').css('display', 'none').fadeIn(3000);
+  });
