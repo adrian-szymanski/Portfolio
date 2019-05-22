6386e022e303c2372dec63bf20828991c32081b8
Adrian Szymanski <adrian.szymanski89@gmail.com>
2019-05-22 22:19:41 +0200

    Making about, work and contact sub-pages
    Plenty of minor changes and updates, polishing, code cleaning, adding tranistion effects and creating new sub-pages.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   });
   
   $(document).ready(function () {
-    $('#mainRow').css('display', 'none').fadeIn(6000);
+    $('#mainRow').css('display', 'none').fadeIn(3000);
   });
 
   $(document).ready(function () {
@@ -15,5 +15,13 @@ $(document).ready(function () {
   });
 
   $(document).ready(function () {
-    $('#parallelogram').css('display', 'none').fadeIn(8000);
+    $('#parallelogram').css('display', 'none').fadeIn(900);
   });
+
+  $(document).ready(function () {
+    $('#parallelogram').animate({width: '1210px', height: '130px', top: '410px'}, 2000);
+  }); 
+
+  $(document).ready(function () {
+    $('.circle').animate({left: '10px'}, 1000);
+  }); 
