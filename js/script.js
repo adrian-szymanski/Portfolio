5f68f1584f82efac727c2a467a9fe3f2054864b3
Adrian Szymanski <adrian.szymanski89@gmail.com>
2019-05-28 20:13:59 +0200

    Clean code
    Code clean-up, CSS formatting fixed;

--- js/script.js
+++ js/script.js
@@ -1,26 +1,19 @@
 $(document).ready(function () {
-    $('#topRow, #mainRow, #bottomBar, #copyright, #info, .arrowDown, .mainContact').css('display', 'none').fadeIn(3000);
-  });
+  $('#topRow, #mainRow, #bottomBar, #copyright, #info, .arrowDown, .mainContact').css('display', 'none').fadeIn(3000);
+});
 
-  $(document).ready(function () {
-    $('.description, .mainWork, .circle, .leftBar').css('display', 'none').fadeIn(1500);
-  });
+$(document).ready(function () {
+  $('.description, .mainWork, .circle, .leftBar').css('display', 'none').fadeIn(1500);
+});
 
-  $(document).ready(function () {
-    $('#parallelogram').css('display', 'none').fadeIn(900);
-  });
+$(document).ready(function () {
+  $('#parallelogram').css('display', 'none').fadeIn(900);
+});
 
-  $(document).ready(function () {
-    $('#parallelogram').animate({width: '70%', height: '10%', top: '410px'}, 2000);
-  }); 
+$(document).ready(function () {
+  $('#parallelogram').animate({width: '70%', height: '10%', top: '410px'}, 2000);
+}); 
 
-  $(document).ready(function () {
-    $('.circle').animate({left: '10px'}, 1000);
-  }); 
-
-  var $container = $('.arrowDown'),
-      $scrollTo = $('#info');
-
-  $container.scrollTop(
-      $scrollTo.offset(10).top - $container.offset(10).top + $container.scrollTop(10)
-  );
+$(document).ready(function () {
+  $('.circle').animate({left: '10px'}, 1000);
+});
