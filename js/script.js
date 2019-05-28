0ae353411490aae7dfcc30e1ba82ec3e4da45f9a
Adrian Szymanski <adrian.szymanski89@gmail.com>
2019-05-28 20:23:05 +0200

    CSS cleaning
    Small CSS updates

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('#topRow, #mainRow, #bottomBar, #copyright, #info, .arrowDown, .mainContact').css('display', 'none').fadeIn(3000);
+  $('.topRow, .mainRow, .bottomBar, .copyright, #info, .arrowDown, .mainContact').css('display', 'none').fadeIn(3000);
 });
 
 $(document).ready(function () {
@@ -7,11 +7,11 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-  $('#parallelogram').css('display', 'none').fadeIn(900);
+  $('.parallelogram').css('display', 'none').fadeIn(900);
 });
 
 $(document).ready(function () {
-  $('#parallelogram').animate({width: '70%', height: '10%', top: '410px'}, 2000);
+  $('.parallelogram').animate({width: '70%', height: '10%', top: '410px'}, 2000);
 }); 
 
 $(document).ready(function () {
